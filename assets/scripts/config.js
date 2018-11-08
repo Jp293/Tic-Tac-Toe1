d4f7673025bd7eec12ba1f305c25ef3141a86e1c
Jp293 <jppr293@gmail.com>
2018-11-08 12:05:23 -0500

    Third phase commit created the api files to sign up, in, out and change passwords nothing modified in previous commit.

--- assets/scripts/config.js
+++ assets/scripts/config.js
@@ -2,8 +2,10 @@
 
 let apiUrl
 const apiUrls = {
-  production: '<replace-with-heroku-url>',
-  development: 'http://localhost:4741'
+  production: 'https://aqueous-atoll-85096.herokuapp.com',
+  development: 'https://tic-tac-toe-wdi.herokuapp.com/'
+
+  // 'http://localhost:4741'
 }
 
 if (window.location.hostname === 'localhost') {
